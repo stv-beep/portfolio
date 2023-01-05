91ccd124eb896c36f5a63b0b06a35291fc0368be
stv-beep <aleixalguero0@gmail.com>
2023-01-05 15:14:50 +0100

    atropos in about me card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import '../App.css';
 import avatar from '../images/aleixalguero.png'
 import ParticlesBG  from './ParticlesBG'
+import 'atropos/css/min'
+import Atropos from 'atropos/react';
 
 export function About(){
     return (
@@ -10,9 +12,9 @@ export function About(){
                     <div className="flex-row">
                         <div>
                             <div className="div-avatar">
-                                <img className="rounded-full m-auto border-solid border-4 border-black w-[270px] aspect-square" src={avatar} alt="Aleix Algueró"/>
+                                <img className="rounded-full m-auto border-solid border-4 border-gray-100 w-[270px] aspect-square" src={avatar} alt="Aleix Algueró"/>
                                 &nbsp;
-                                <div className="p-2 bg-gray-300 rounded-lg mt-0">
+                                <div className="p-2 bg-gray-200 rounded-lg mt-0">
                                     <span className="social-links">
                                         <a href="https://www.linkedin.com/in/aleix-alguero-omedes" target="_blank" className="btn">
                                             <i className="devicon-linkedin-plain"></i></a>
@@ -22,28 +24,25 @@ export function About(){
                             </div>
                         </div>
                         
-                        <div className="about-info">
-                            <div>
-                                <div className="box">
-                                    <div className="py-3">
-                                        <p className="montserrat font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-200 via-green-400 to-green-600">ALEIX ALGUERÓ</p>
-                                        <span className="font-medium bg-clip-text text-transparent bg-gradient-to-r from-green-200 via-green-300 to-green-400">
-                                        Web Developer and SysAdmin</span>
-                                    </div>
-                                    <p className='italic text-xl bg-clip-text text-transparent bg-gradient-to-r
-                                     from-green-200 via-green-300 to-green-400'>
-                                        "Always trying to improve myself and those around me."</p>
-                                    <br/>
-                                    <p className="text-lg montserrat text-white text-justify">
-                                        Technology enthusiast. I started repairing computers and realized that I loved this. 
-                                        Then I studied Computer Network Systems Administration and I also liked it, 
-                                        and then Web Development, which I liked even more. I enjoy helping people 
-                                        by solving IT problems that they may have or developing their ideas through an app.
-                                    </p>
-                                    
+                        <Atropos className="about-info">
+                            <div className="box">
+                                <div className="py-3 lg:contrast-200">
+                                    <p className="montserrat font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-200 via-green-400 to-green-600" data-atropos-offset='2'>ALEIX ALGUERÓ</p>
+                                    <span className="font-medium bg-clip-text text-transparent bg-gradient-to-r from-green-200 via-green-300 to-green-400" data-atropos-offset='4'>
+                                    Web Developer and SysAdmin</span>
                                 </div>
+                                <p className='italic text-xl bg-clip-text text-transparent bg-gradient-to-r
+                                    from-green-200 via-green-300 to-green-400 lg:contrast-200' data-atropos-offset='5'>
+                                    "Always trying to improve myself and those around me."</p>
+                                <br/>
+                                <p className="xl:text-lg sm:text-sm montserrat text-white text-justify aboutme" data-atropos-offset='2'>
+                                    Technology enthusiast. I started repairing computers and realized that I loved this. 
+                                    Then I studied Computer Network Systems Administration and I also liked it, 
+                                    and then Web Development, which I liked even more. I enjoy helping people 
+                                    by solving IT problems that they may have or developing their ideas through an app.
+                                </p>
                             </div>
-                        </div>
+                        </Atropos>
                     </div>
                 </div>
             </section>
