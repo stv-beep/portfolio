b0b79574fb7f9977739e41e149bf0bd3fd457380
stv-beep <aleixalguero0@gmail.com>
2023-02-07 16:58:03 +0100

    change at responsive navbar btn, about me text and bg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -28,30 +28,37 @@ const MenuItems = [
 
 
 export function Nav() {
-  return (
-    <nav className="nav sticky top-0 z-10 backdrop-blur-sm">
-      <input type="checkbox" id="nav-check" />
-      <div className="nav-header">
-        <div className="nav-title">
-          <a href="/" className="bg-clip-text text-transparent bg-gradient-to-r from-red-600 via-red-400 to-yellow-400 ml-4 hover:text-white">{name}</a>
-        </div>
+  return (<nav className="nav sticky top-0 z-10 backdrop-blur-sm">
+    <input type="checkbox" id="nav-check" />
+    <div className="nav-header">
+      <div className="nav-title">
+        <a href="/" className="bg-clip-text text-transparent bg-gradient-to-r from-red-600 via-red-400 to-yellow-400 ml-4 hover:text-white">{name}</a>
       </div>
-      <div className="nav-btn">
-        <label htmlFor="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
-      <div className="nav-links pr-4" id="navItems">
-        {MenuItems.map((item, index) => {
-          return (
-            <a href={item.url} key={index} className="bg-clip-text text-transparent  bg-gradient-to-r from-yellow-100 via-yellow-200 to-yellow-600 hover:text-white">
-              {item.Title}
-            </a>
-          );
-        })}
-      </div>
-    </nav>
+    </div>
+    <div className="nav-btn">
+      <label htmlFor="nav-check">
+      <span className='displayMenu'>
+        <div></div>
+        <div></div>
+        <div></div>
+      </span>
+
+      </label>
+    </div>
+
+
+
+    <div className="nav-links pr-4" id="navItems">
+      {MenuItems.map((item, index) => {
+        return (
+          <a href={item.url} key={index} className="bg-clip-text text-transparent  bg-gradient-to-r from-yellow-100 via-yellow-200 to-yellow-600 hover:text-white">
+            {item.Title}
+          </a>
+        );
+      })}
+    </div>
+  </nav>
+
+
   )
 }
